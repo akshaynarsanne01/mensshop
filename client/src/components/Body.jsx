3845d04eeb5423e0ee942f1aa5e1c01ee900fe7d
Akshay Narsanne <akshaynarsanne01@gmail.com>
2024-05-29 01:36:46 +0530

    Restructured Routes

--- client/src/components/Body.jsx
+++ client/src/components/Body.jsx
@@ -1,28 +1,13 @@
-// import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Brand from './Brand';
-import Dashboard from './Dashboard';
 import AdminMenu from "./AdminMenu";
-import Category from './Category';
-import Subcategory from './Subcategory';
-import Products from './products';
+import { Outlet } from 'react-router-dom';
 const App = () => {
   return (
-    <Router>
-      <div className="body">
-        <AdminMenu />
-        <div className="body-content">
-          <Routes>
-            <Route path="/admin" element={<Dashboard/>} />
-            <Route path='/admin/Brand' element={<Brand/>}></Route>
-            <Route path='/admin/Brand/:id' element={<Brand/>}></Route>
-            <Route path="/admin/category" element={<Category/>} />
-            <Route path="/admin/subcategory" element={<Subcategory/>} />
-            <Route path="/admin/products" element={<Products/>} />
-          </Routes>
-        </div>
+    <div className="body">
+      <AdminMenu />
+      <div className="body-content">
+        <Outlet />
       </div>
-    </Router>
+    </div>
   );
 }
 
--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,7 +1,33 @@
-import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
+import Dashboard from './components/Dashboard.jsx';
+import Brand from './components/Brand.jsx';
+import Category from "./components/Category.jsx"
+import SubCategory from "./components/Subcategory.jsx";
+import Products from "./components/products.jsx";
+import ErrorPage from "./components/ErrorPage.jsx"
 import './index.css'
+import * as ReactDOM from "react-dom";
+import {
+    createBrowserRouter,
+    RouterProvider,
+} from "react-router-dom";
 
+const router = createBrowserRouter([
+    {
+        path: "/",
+        element: <App />,
+        errorElement: <ErrorPage />,
+        children: [
+            { path: "/", element: <Dashboard /> },
+            { path: "admin", element: <Dashboard /> },
+            { path: "admin/Brand", element: <Brand /> },
+            { path: "admin/Brand/:id", element: <Brand /> },
+            { path: "admin/category", element: <Category /> },
+            { path: "admin/subcategory", element: <SubCategory /> },
+            { path: "admin/products", element: <Products /> }
+        ]
+    }
+]);
 ReactDOM.createRoot(document.getElementById('root')).render(
-    <App />
+    <RouterProvider router={router} />
 )
